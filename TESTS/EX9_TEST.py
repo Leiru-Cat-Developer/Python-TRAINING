26b4f30b1e6ab331d64de4fe53a0191d2435d488
Uriel Maldonado <blowitup2001@gmail.com>
2025-09-23 18:09:41 -0600

    Another TESTS

---
+++ TESTS/EX9_TEST.py
@@ -0,0 +1,20 @@
+# BINARY SEARCH
+
+def binarySearch(array: list[int], number: int):
+    array.sort()
+    print("\n" + str(array))
+    leftPos = 0
+    rightPos = len(array)-1
+    while leftPos <= rightPos:
+        half = (leftPos + rightPos)//2
+        if array[half] == number:
+            return f"\nThe number exists in position: {str(half)}"
+        elif array[half] > number:
+            rightPos = half - 1
+        elif array[half] < number:
+            leftPos = half + 1
+    return "\nThe number doesn't exists"
+
+array = [5,7,4,10,54,3,69,11,52,30]
+number = 10
+print(binarySearch(array,number))
